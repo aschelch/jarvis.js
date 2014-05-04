8214113949a758fb72b0aa3f6dd26e38e2101a14
Aurélien Schelcher <a.schelcher@ubitransports.com>
2014-05-04 22:31:29 +0200

    Add t411 torrent module

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-var exec = require('child_process').exec;
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .option('-v, --verbose', 'display logs')
+  .parse(process.argv);
 
 var speak = require('./modules/speak.js');
 var weather = require('./modules/weather.js');
 var example = require('./modules/example.js');
+var torrent = require('./modules/torrent.js');
 
-console.log("Starting Jarvis...");
+if(program.verbose) console.log("Starting Jarvis...");
 
 speak.start();
 weather.start();
 example.start();
+torrent.start();
 
-console.log("Jarvis started");
-
+if(program.verbose) console.log("Jarvis started");
