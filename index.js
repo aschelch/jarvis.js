1ef4a11f7ab7a3e7e3c78db0b18d830bb65d05b1
Aurélien Schelcher <a.schelcher@ubitransports.com>
2014-05-09 14:36:48 +0200

    Add terminal plugin, and refactor

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+
+var program = require('commander');
+
 var Jarvis = require('./lib/jarvis');
+
+
+program
+  .version('1.0.0')
+  .option('-v, --voice [Loic]', 'Voxygen voice to use in tts (Loic, Agnes, Melodine, Chut, Bicool, Philippe, Electra, Damien, DarkVadoor, Ramboo, John, Helene, Eva, JeanJean, Papi, Robot, Sidoo, Sorciere, Yeti, Zozo)', 'Loic')
+  .parse(process.argv);
+
 
 var jarvis = new Jarvis();
 jarvis.start()
